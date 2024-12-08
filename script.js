deff4a3f4b02a71a29aa7b270e4cb6f92e484da0
StefanMogren <stefan_mogren@hotmail.com>
2024-12-08 16:07:26 +0100

    Finished functionality for no symbols or numbers

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-const submitBtn = document.getElementById("submit-button");
 const inputPasswordLength = document.getElementById("password-length");
+
+const submitBtn = document.getElementById("submit-button");
+submitBtn.addEventListener('click', generatePassword)
+
 const outputOne = document.getElementById("output-one");
 const outputTwo = document.getElementById("output-two");
-
-
-let noSymbols = true;
-let noNumbers = true;
-
-submitBtn.addEventListener('click', generatePassword)
 outputOne.addEventListener('click', () => { copyPassword(outputOne); })
 outputTwo.addEventListener('click', () => { copyPassword(outputTwo); })
+
+const symbolsCheckbox = document.getElementById("symbols-checkbox");
+const numbersCheckbox = document.getElementById("numbers-checkbox");
+let noSymbols = false;
+let noNumbers = false;
+symbolsCheckbox.addEventListener('change', function() {
+    noSymbols = !noSymbols
+})
+numbersCheckbox.addEventListener('change', function() {
+    noNumbers = !noNumbers
+})
 
 function copyPassword(output) {
     if (output.textContent !== "") {
         navigator.clipboard.writeText(output.textContent)
     }
 }
-
-// if (/[^a-zA-Z0-9]/.test("¤")) {
-//     console.log("It's a symbol!")
-// } else {
-//     console.log("It isn't a symbol...")
-// }
-// if(isNaN("{")) {
-//     console.log("It isn't a number...")
-// } else {
-//     console.log("It's a number!")
-// }
-
 
 function generatePassword() {
     let length = Math.floor(inputPasswordLength.value);
@@ -61,17 +57,3 @@ function getCharacter() {
     }
     return characters[randomNumber];
 };
-
-
-// console.log(characters[randomNumber])
-
-// if (noSymbols === true && /[^a-zA-Z0-9]/.test(characters[randomNumber]) === true) {
-//     console.log("It didn't pass through and said: " + /[^a-zA-Z0-9]/.test(characters[randomNumber]));
-//     getCharacter();
-// } else if (noNumbers === true && isNaN(characters[randomNumber]) === false) {
-//     getCharacter();
-// } else {
-//     console.log("It passed through and said:" + /[^a-zA-Z0-9]/.test(characters[randomNumber]));
-//     console.log(characters[randomNumber])
-//     return characters[randomNumber];
-// }
